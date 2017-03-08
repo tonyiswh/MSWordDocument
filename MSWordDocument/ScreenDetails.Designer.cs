7cae1d039380d15b996cec07188af84f7d63c9b1
tonyl <tonyl@PC004>
2017-03-08 12:56:24 +1100

    Update ScreenDatails

---
+++ MSWordDocument/ScreenDetails.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace MSWordDocument
+{
+    partial class ScreenDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.OpenPopup = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(281, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(28, 81);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(345, 184);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
+            // 
+            // Column1
+            // 
+            this.Column1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            this.Column1.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column1.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            // 
+            // OpenPopup
+            // 
+            this.OpenPopup.Location = new System.Drawing.Point(310, 280);
+            this.OpenPopup.Name = "OpenPopup";
+            this.OpenPopup.Size = new System.Drawing.Size(75, 23);
+            this.OpenPopup.TabIndex = 2;
+            this.OpenPopup.Text = "Open";
+            this.OpenPopup.UseVisualStyleBackColor = true;
+            this.OpenPopup.Click += new System.EventHandler(this.OpenPopup_Click);
+            // 
+            // ScreenDetails
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(414, 315);
+            this.Controls.Add(this.OpenPopup);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Name = "ScreenDetails";
+            this.Text = "Screen";
+            this.Load += new System.EventHandler(this.ScreenDetails_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Button OpenPopup;
+    }
+}
---
+++ MSWordDocument/ScreenDetails.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MSWordDocument
+{
+    public partial class ScreenDetails : Form
+    {
+        public ScreenDetails()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (var screen in System.Windows.Forms.Screen.AllScreens)
+            {
+                if (screen.Primary)
+                {
+                    var width = screen.WorkingArea.Width;
+                    var height = screen.WorkingArea.Height;
+                }
+            }
+
+           
+        }
+
+        private void ScreenDetails_Load(object sender, EventArgs e)
+        {
+           
+            List<string> dbTypeSQLServerList = new List<string>()
+            {
+                "Binary",
+                "Byte",
+                "Boolean",
+                "Currency",
+                "Date",
+                "DateTime",
+                "Decimal",
+                "Double",
+                "Guid",
+                "Int16",
+                "Int32",
+                "Int64",
+                "Object",
+                "SByte",
+                "Single",
+                "String",
+                "Time",
+                "UInt16",
+                "UInt64",
+            };
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Column11");
+            dt.Columns.Add("Column22");
+            dt.Columns.Add("Column33");
+
+            DataRow dr = dt.NewRow();
+            dr["Column11"] = "Object";
+            dr["Column22"] = "r12";
+            dr["Column33"] = "r13";
+            dt.Rows.Add(dr);
+
+            DataRow dr2 = dt.NewRow();
+            dr2["Column11"] = "Int64";
+            dr2["Column22"] = "r22";
+            dr2["Column33"] = "r23";
+            dt.Rows.Add(dr2);
+
+            
+            dataGridView1.Rows.Clear();
+            dataGridView1.AutoGenerateColumns = false;
+
+            var col1 = (DataGridViewComboBoxColumn)dataGridView1.Columns["Column1"];
+            col1.DataSource = dbTypeSQLServerList;
+            //col1.DisplayMember = "Value";
+            //col1.ValueMember = "Key";
+
+            dataGridView1.Columns["Column1"].DataPropertyName = "Column11";
+            dataGridView1.Columns["Column2"].DataPropertyName = "Column22";
+            dataGridView1.Columns["Column3"].DataPropertyName = "Column33";
+                       
+            dataGridView1.DataSource = dt;
+            dataGridView1.Refresh();
+
+            SetTextColor();
+        }
+
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if ( e.ColumnIndex == 0 && e.RowIndex >= 0 )
+            {
+                var value = dataGridView1.Rows[e.RowIndex].Cells["Column1"].Value;
+                if (value.ToString().ToLower() ==  "object")
+                {
+                    dataGridView1.Rows[e.RowIndex].Cells["Column1"].Style.ForeColor = Color.Red;
+                }
+                else
+                {
+                    dataGridView1.Rows[e.RowIndex].Cells["Column1"].Style.ForeColor = Color.Black;
+                }
+            }
+        }
+
+
+        private void SetTextColor()
+        {
+            for(int r = 0 ; r < dataGridView1.Rows.Count - 1 ; r++)
+            {
+                var value = dataGridView1.Rows[r].Cells["Column1"].Value;
+                if (value.ToString().ToLower() == "object")
+                {
+                    dataGridView1.Rows[r].Cells["Column1"].Style.ForeColor = Color.Red;
+                    dataGridView1.Rows[r].Cells["Column1"].Selected = false;
+                }
+            }
+
+           
+
+        }
+
+        private void OpenPopup_Click(object sender, EventArgs e)
+        {
+            DataSetFields dsFields = new DataSetFields();
+            dsFields.DatasetName = "First1";
+
+            WPField wpField = new WPField();
+            wpField.FieldName = "Field1";
+            wpField.DataType = DbType.Int32;
+
+            dsFields.Fields.Add(wpField);
+
+            WPField wpField2 = new WPField();
+            wpField2.FieldName = "Field2";
+            wpField2.DataType = DbType.String;
+
+            dsFields.Fields.Add(wpField2);
+
+            WPField wpField3 = new WPField();
+            wpField3.FieldName = "Field3";
+            wpField3.DataType = DbType.String;
+
+            dsFields.Fields.Add(wpField3);
+
+            Popup popup = new Popup(dsFields);
+            popup.ShowDialog();
+
+
+        }
+    }
+
+    public class DataSetFields
+    {
+        private List<WPField> _fields = new List<WPField>();
+
+        public List<WPField> Fields
+        {
+            get { return _fields; }
+            set { _fields = value; }
+        }
+
+        public string DatasetName { get; set; }
+    }
+
+    public class WPField
+    {
+        public string FieldName { set; get; }
+
+        public DbType DataType { get; set; }
+
+        public bool Selected { get; set; }
+    }
+
+}
